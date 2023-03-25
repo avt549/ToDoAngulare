7862d143919b596b1a11f35e8f6fc14837c1b13e
avt549 <avt549@yandex.ru>
2023-03-25 13:18:13 +0300

    Добавил библиотеки bootstrap и material

---
+++ src/app/model/Task.ts
@@ -0,0 +1,22 @@
+import {Priority} from "./Priority";
+import {Category} from "./Category";
+import {DatePipe} from "@angular/common";
+import {Data} from "@angular/router";
+export class Task{
+  id: number;
+  title: string;
+  completed: boolean;
+  priority?: Priority;
+  category?: Category;
+  date?: Data;
+
+
+  constructor(id: number, title: string, completed: boolean, priority?: Priority, category?: Category, date?: Data) {
+    this.id = id;
+    this.title = title;
+    this.completed = completed;
+    this.priority = priority;
+    this.category = category;
+    this.date = date;
+  }
+}
